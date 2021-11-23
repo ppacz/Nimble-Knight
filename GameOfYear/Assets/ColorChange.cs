ebe194eccab3eae0dd1e6dfe23834d6d674e0e4b
Tomáš Lipenský <ppacz@email.cz>
2021-11-23 14:55:14 +0100

    Made better menu; added changing background

---
+++ GameOfYear/Assets/ColorChange.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColorChange : MonoBehaviour
+{
+    public Image img;
+    public int changeSpeed;
+    public float red;
+    public float blue;
+    int timer = 0;
+    float green;
+    
+    bool change = true;
+    
+    private void FixedUpdate()
+    {
+        if (timer % changeSpeed==0) colorChange();
+        timer++;
+        if (timer > 100) timer = 0;
+    }
+
+    void colorChange() 
+    {
+            img.color = new Color(red, green / 100, blue);
+            if (green == 100) change = false;
+            else if (green == 0) change = true;
+            if (change) green++;
+            else green--;
+    }
+
+}
