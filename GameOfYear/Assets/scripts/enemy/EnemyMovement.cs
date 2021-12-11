e3e79cbfcf0d8b60aa90cc398ca93f255fe89d42
Vojtěch L <vojta.lyson@seznam.cz>
2021-12-11 19:42:25 +0100

    added EnemyMovement (not working)

---
+++ GameOfYear/Assets/scripts/enemy/EnemyMovement.cs
@@ -0,0 +1,30 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyMovement : MonoBehaviour 
+{
+    public Rigidbody2D rb;
+    public MainHero player;
+    private float moveSpeed;
+    private Vector3 directionToPlayer;
+    private Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType(typeof(MainHero)) as MainHero;
+        moveSpeed = 2f;
+        localScale = transform.localScale;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
+*/
