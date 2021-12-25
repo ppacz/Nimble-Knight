d8b46714648c379f06396739e09e99c74034d5c9
Tomáš Lipenský <ppacz@email.cz>
2021-12-25 20:05:34 +0100

    added pathfinding of enemy

---
+++ GameOfYear/Assets/scripts/enemy/EnemyAI.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 10f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int currnetWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+
+    }
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currnetWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(path == null) return;
+        
+        if (currnetWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+        Vector2 direction = ((Vector2)path.vectorPath[currnetWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currnetWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currnetWaypoint++;
+        }
+    }
+}
