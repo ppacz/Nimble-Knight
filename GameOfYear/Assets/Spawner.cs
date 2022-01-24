3a6f006f6e76e234b22ba26ed8b95bc897a92a2a
Tomáš Lipenský <ppacz@email.cz>
2022-01-24 20:36:15 +0100

    Multiple spawners
    Parent spawner shouldn't be included at all,  need to make spawning asynchronous or mby not dont know yet

--- GameOfYear/Assets/Spawner.cs
+++ GameOfYear/Assets/Spawner.cs
@@ -15,9 +15,10 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private LayerMask nonSpawnableLayers;//layers where enemy shouldnt be able to spawn at all
     
-    private List<GameObject> enemies = new List<GameObject>();
-    private float time;
-
+    //private List<GameObject> enemies = new List<GameObject>();
+    //private float time;
+    private List<List<GameObject>> enemiesList = new List<List<GameObject>>();
+    private Transform[] spawnPoints;
     #region singleton
     public static Spawner instance;
 
@@ -30,8 +31,16 @@ public class Spawner : MonoBehaviour
 
     private void Start()
     {
-        time = timeBetweenSpawns;
+        //time = timeBetweenSpawns;
+        spawnPoints = gameObject.GetComponentsInChildren<Transform>(false);
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            enemiesList.Add(new List<GameObject>());
+        }
+        InvokeRepeating("Spawning", 5f, timeBetweenSpawns);
     }
+    /*
+    // this one is one that works with single spanwer
     private void Update()
     {
         time -= Time.deltaTime;
@@ -45,10 +54,19 @@ public class Spawner : MonoBehaviour
 
             
         }
+    }*/
+    private void Spawning()
+    {
+        for(int i = 0; i < enemiesList.Count; i++)
+        {
+            while (enemiesList[i].Count < amountOfEnemies)
+            {
+                createEnemy(spawnPoints[i], enemiesList[i]);
+            }
+        }
     }
 
-
-
+    /*
     private void createEnemy()
     {
         float rx = Random.Range(-10f, 10f);
@@ -65,17 +83,43 @@ public class Spawner : MonoBehaviour
         }
         GameObject enemy = (GameObject)Instantiate(enemyToSpawn, position, new Quaternion(0,0,0,0));
         enemies.Add(enemy);
+    }*/
+    /*
+     * this one should work with more than one spawner, each spawner needs to have same parent
+     */
+    private void createEnemy(Transform spawnPoint,List<GameObject> enemies)
+    {
+        int tryes = 0;
+        float rx = Random.Range(-10f, 10f);
+        float ry = Random.Range(-10f, 10f);
+        Vector3 position = new Vector3(spawnPoint.position.x + rx, spawnPoint.position.y + ry);
+        Collider2D[] hitObstacle;
+        hitObstacle = Physics2D.OverlapCircleAll(position, 1f, nonSpawnableLayers);
+        while (hitObstacle.Length != 0)
+        {
+            if (tryes > 10) return; 
+            rx = Random.Range(-5f, 5f);
+            ry = Random.Range(-5f, 5f);
+            position = new Vector3(spawnPoint.position.x + rx, spawnPoint.position.y + ry);
+            hitObstacle = Physics2D.OverlapCircleAll(position, 1f, nonSpawnableLayers);
+            tryes++;
+        }
+        GameObject enemy = (GameObject)Instantiate(enemyToSpawn, position, new Quaternion(0, 0, 0, 0));
+        enemies.Add(enemy);
     }
+
+
+
+    /*public void deleteEnemy(GameObject enemy)
+    {
+            enemies.Remove(enemy);
+    }*/
 
     public void deleteEnemy(GameObject enemy)
     {
-        try
+        foreach(List<GameObject> enemies in enemiesList)
         {
             enemies.Remove(enemy);
-        }
-        catch
-        {
-            Debug.Log("This enemy wasn't spawned by spawner");
         }
     }
 }
