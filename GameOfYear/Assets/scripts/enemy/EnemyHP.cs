6b51235f7688f3946660ccf871f8418d836df4df
Tomáš Lipenský <ppacz@email.cz>
2022-03-18 23:02:30 +0100

    Cleaned some code

--- GameOfYear/Assets/scripts/enemy/EnemyHP.cs
+++ GameOfYear/Assets/scripts/enemy/EnemyHP.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHP : MonoBehaviour
@@ -11,7 +9,7 @@ public class EnemyHP : MonoBehaviour
     {
         currentHealth = maxHealt;
     }
-
+    
     public void getsDamaged(int dmg)
     {
         currentHealth -= dmg;
--- GameOfYear/Assets/scripts/scriptsForMenu/ColorChange.cs
+++ GameOfYear/Assets/scripts/scriptsForMenu/ColorChange.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class ColorChange : MonoBehaviour
 {
